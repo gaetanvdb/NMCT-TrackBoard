daab2812fa5ee14507565fd98d7cc221e49de34c
Gaetan Van Den Berge <gaetan.van.den.berge@student.howest.be>
2017-06-14 17:13:28 +0200

    login

--- model/gpsData.py
+++ model/gpsData.py
@@ -8,24 +8,26 @@ ser = serial.Serial(port, baudrate, timeout=timeout)
 
 def getGpsData():
     status2 = 0
+    status1 = 0
     gpsDataList = []
-    while status2 == 0:
+    # voor altitude
+    while status1 == 0:
         line = str(ser.readline())
         data = line.split(",")
         if data[0] == "b'$GPGGA":
-            print("------------------")
             gpsDataList.append(data[9])  #altitude --> 0
             gpsDataList.append(data[6]) #gpsFix --> 1
             gpsDataList.append(data[7]) #usedSat --> 2
             status1 = 1
+    # voor Time, date, latitude, longitude, speed
+    while status2 == 0:
+        line = str(ser.readline())
+        data = line.split(",")
         if data[0] == "b'$GPRMC":
             gpsDataList.append(data[1]) #time --> 3
             gpsDataList.append(data[9]) #date --> 4
             gpsDataList.append(data[3]) #latitude --> 5
             gpsDataList.append(data[5]) #longitude --> 6
-            speed = data[7] #speed --> 7
+            gpsDataList.append(data[7]) #speed --> 7
             status2 = 1
     return gpsDataList
-
-print(getGpsData())
-;
--- sessionLogger.py
+++ sessionLogger.py
@@ -1,6 +1,79 @@
 from DbClass import DbClass
-
 db = DbClass()
 
-db.setNewSession('2010-10-10', '10:00:00')
-db.updateSession('11:00:00')
+#db.setNewSession('2010-10-10', '10:00:00')
+#db.updateSession('11:00:00')
+
+import RPi.GPIO as GPIO
+import time
+import sys
+from model import gpsData
+
+GPIO.setwarnings(False)
+
+drukknop  = 21
+ledGroen = 16
+ledRood = 6
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledGroen, GPIO.OUT)
+GPIO.setup(ledRood, GPIO.OUT)
+GPIO.setup(drukknop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+button='up'
+session='NOT-recording'
+list = []
+
+try:
+    while True:
+        if (button=='up'   and   session == 'NOT-recording'):
+            # wait for button press before changing anything
+            if not GPIO.input(drukknop):
+                GPIO.output(ledRood, 1)
+                GPIO.output(ledGroen, 0)
+                button='down'
+                session = 'recording'
+
+        elif (button=='down' and   session=='recording'):
+            # stay in this state until button released
+            if GPIO.input(drukknop):
+                button='up'
+
+        elif (button=='up' and session=='recording'):
+            if not GPIO.input(drukknop):
+                GPIO.output(ledGroen, 1)
+                GPIO.output(ledRood, 0)
+                # Sessie recording ------------
+                time.sleep(1)
+                while GPIO.input(drukknop) == 1:
+                    knop = GPIO.input(drukknop)
+                    db.setNewGpsLine(gpsData.getGpsData()[3], gpsData.getGpsData()[5], gpsData.getGpsData()[6], gpsData.getGpsData()[7], "99", gpsData.getGpsData()[0], "8")
+                    print("insert succesvol")
+                    time.sleep(0.5)
+                #-----------------------------
+                #Rode led laten branden zodat gebruiker weet dat recording gestopt is -------
+                GPIO.output(ledGroen, 0)
+                GPIO.output(ledRood, 0)
+                # -----------------------------
+                # Data verwerken -----------------------------
+                print(list)
+                list.clear()
+                # -----------------------------
+                button = 'down'
+                session = 'NOT-recording'
+                for i in range(5):
+                    GPIO.output(ledRood, 1)
+                    time.sleep(0.2)
+                    GPIO.output(ledRood, 0)
+                    time.sleep(0.2)
+
+                sys.exit("Gedaan")
+        elif (button=='down' and session=='NOT-recording'):
+            if GPIO.input(drukknop):
+                button='up'
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print("KeyboardInterrupt detected")
+    GPIO.output(ledGroen, GPIO.LOW)
+    GPIO.output(ledRood, GPIO.LOW)
+    GPIO.cleanup()
